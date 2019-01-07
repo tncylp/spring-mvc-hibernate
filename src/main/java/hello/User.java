d7769ac300a6371f0994c759f605092efafa0129
Tuncay Alp Emekci <tuncayemekci@gmail.com>
2019-01-07 23:56:48 +0300

    Some trials

--- src/main/java/hello/User.java
+++ src/main/java/hello/User.java
@@ -1,10 +1,6 @@
 package hello;
 
-import java.io.Serializable;
-
-public class User implements Serializable{
-
-    private static final long serialVersionUID = 1L;
+public class User{
 
     private int id;
     private String name;
